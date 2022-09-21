c165f8fe150d90d000bdb3405814a0da5f73c068
vamsiPR12 <113491972+vamsiPR12@users.noreply.github.com>
2022-09-21 13:42:06 +0530

    Add files via upload

---
+++ vote eligibility.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+	int a ;
+	
+	//input age
+	printf("Enter the age of the person: ");
+	scanf("%d",&a);
+
+	//check voting eligibility
+	if (a>=18)
+	{
+		printf("Eigibal for voting");
+	}
+	else
+	{
+		printf("Not eligibal for voting\n");
+	}
+	return 0;
+
